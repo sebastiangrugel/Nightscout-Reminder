8fcaf11cea9a2348eff46c5a67d0bf1c695fa897
Sebastian Grugel <sebastian.grugel@gmail.com>
2021-05-02 13:30:25 +0200

    regex resolved by Darek

--- api_request_nightscout_calibration_notification_v2.py
+++ api_request_nightscout_calibration_notification_v2.py
@@ -12,7 +12,7 @@ print("RAW data from JSON:",status)
 
 # Section related to convert time from JSON to readable value
 import re
-match = re.match('.* ([0-9])+h([0-9])+m.*', status) # in this line removed space before 2nf dot... (fix)
+match = re.match('.* ([0-9]+)h([0-9]+)m.*', status) # in this line removed space before 2nf dot... (fix)
 hours, minutes = match.groups()
 print(f"Information converted from JSON by REGEX: {hours} hours and {minutes} minutes")
 
@@ -37,29 +37,39 @@ def post_to_slack(message, credentials):
     url = get_credentials(credentials)
     requests.post(url, json=data, verify=False)
 
-# Conditions
+
+
+########### TEST SECTION ###############
 
 # Send information about time to calibration every time when code is run. Comment this section if not need it.
-print("Message sent to SLACK")
-post_to_slack(message, credentials)
+#print("Message sent to SLACK")
+#post_to_slack(message, credentials)
 ##############################################################################################################
 
+if int(hours) == 6: # and int(minutes) == 0:
+        print(f"{hours} hours")
+        print("Message sent to SLACK")
+        post_to_slack(message, credentials)
 
-while int(hours) == 2:
+
+##############################################################################################################
+
+# Conditions
+
+if int(hours) == 2:
         print(f"more than {hours} hours")
         print("Message sent to SLACK")
         post_to_slack(message, credentials)
-        time.sleep(1800) #repeat message every
 
-while int(hours) == 1:
-        print("more than 2 hours", int(hours))
-        print("Message sent to SLACK")
-        post_to_slack(message, credentials)
-        time.sleep(900)
-
-while int(hours) < 1:
+if int(hours) == 1:
         print("more than 1 hours", int(hours))
         print("Message sent to SLACK")
         post_to_slack(message, credentials)
-        time.sleep(600)
+
+''' Temporary not working
+if int(hours) < 1:
+        print("less than 1 hours", int(hours))
+        print("Message sent to SLACK")
+        post_to_slack(message, credentials)
+'''
 
