d5fb84fe53d5b4f4fa39ed3b922915e120c247d0
Sebastian Grugel <sebastian.grugel@gmail.com>
2021-05-02 23:48:01 +0200

    test

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 import os
+import requests
+
 print("Plik testowy")
 
-os.environ['haslo'] = 'tajneprzezpoufne'
+#os.environ['haslo'] = 'https://hooks.slack.com/services/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'
 
-zmienna = os.environ.get('haslo')
+#zmienna = os.environ.get('haslo')
 
-print(zmienna)
+#print(zmienna)
 #test
+
+
+import json
+
+
+credentials = os.environ.get('haslo')
+message = f"Test SLACKA ==> tajne {credentials}"
+
+
+def post_to_slack(message, credentials):
+    data = {'text': message}
+    url = credentials
+    requests.post(url, json=data, verify=False)
+
+print("Message sent to SLACK")
+post_to_slack(message, credentials)
