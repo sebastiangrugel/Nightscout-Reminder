99eb1e530da0cf9d738f5e636f5935127b85982e
Sebastian Grugel <sebastian.grugel@gmail.com>
2021-05-06 02:12:19 +0200

    time hours minutes

--- api_request_nightscout_calibration_notification_v2.py
+++ api_request_nightscout_calibration_notification_v2.py
@@ -4,9 +4,9 @@ import os
 import sys
 
 # Get RAW data from Nightscout related to calibration time
-URL = 'https://grugelki-klikemia-jan.herokuapp.com/api/v1/devicestatus.json?find[device]=medtronic-600://640G&count=1' #LatestfromSony
+#URL = 'https://grugelki-klikemia-jan.herokuapp.com/api/v1/devicestatus.json?find[device]=medtronic-600://640G&count=1' #LatestfromSony
 #URL = 'https://grugelki-klikemia-jan.herokuapp.com/api/v1/devicestatus.json?find[_id]=6064f94a4c8d1c0004b1cf3b' #8h55m
-#URL = 'https://grugelki-klikemia-jan.herokuapp.com/api/v1/devicestatus.json?find[_id]=608fb555bed68000049e5fc7' #95m
+URL = 'https://grugelki-klikemia-jan.herokuapp.com/api/v1/devicestatus.json?find[_id]=608fb555bed68000049e5fc7' #95m
 
 # GET data releted to pump calibration time from JSON
 response = requests.get(URL)
